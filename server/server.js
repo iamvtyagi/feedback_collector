68d4f54c3fbf0e2e3ddbdf2b60f968de445ce4ab
Vansh Tyagi <vanshjii021@gmail.com>
2025-04-25 22:02:07 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -18,9 +18,9 @@ const app = express();
 app.use(
   cors({
     origin: "*", // Allow all origins
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+    // allowedHeaders: ["Content-Type", "Authorization"],
+    // credentials: true,
   })
 );
 app.use(express.json());
